598dc056a3d0de765fd79046572e58e8e5a569a4
Brunno Hofmann <brunnohofmann@hotmail.com>
2020-07-07 20:58:23 -0300

    Just adds a Google analytics plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,49 +6,62 @@ import Contact from './Contact'
 import Footer from './Footer'
 
 import '../assets/scss/main.scss'
+import Helmet from 'react-helmet'
 
 class Layout extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isMenuVisible: false,
-            loading: 'is-loading'
-        }
-        this.handleToggleMenu = this.handleToggleMenu.bind(this)
+  constructor(props) {
+    super(props)
+    this.state = {
+      isMenuVisible: false,
+      loading: 'is-loading',
     }
+    this.handleToggleMenu = this.handleToggleMenu.bind(this)
+  }
 
-    componentDidMount () {
-        this.timeoutId = setTimeout(() => {
-            this.setState({loading: ''});
-        }, 100);
+  componentDidMount() {
+    this.timeoutId = setTimeout(() => {
+      this.setState({ loading: '' })
+    }, 100)
+  }
+
+  componentWillUnmount() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId)
     }
+  }
 
-    componentWillUnmount () {
-        if (this.timeoutId) {
-            clearTimeout(this.timeoutId);
-        }
-    }
+  handleToggleMenu() {
+    this.setState({
+      isMenuVisible: !this.state.isMenuVisible,
+    })
+  }
 
-    handleToggleMenu() {
-        this.setState({
-            isMenuVisible: !this.state.isMenuVisible
-        })
-    }
+  render() {
+    const { children } = this.props
 
-    render() {
-        const { children } = this.props
+    return (
+      <>
+        <Helmet>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-44489810-6"></script>
+          <script>{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-        return (
-            <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
-                <div id="wrapper">
-                    <Header onToggleMenu={this.handleToggleMenu} />
-                    {children}
-                    <Footer />
-                </div>
-                <Menu onToggleMenu={this.handleToggleMenu} />
-            </div>
-        )
-    }
+            gtag('config', 'UA-44489810-6');
+        `}</script>
+        </Helmet>
+        <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
+          <div id="wrapper">
+            <Header onToggleMenu={this.handleToggleMenu}/>
+            {children}
+            <Footer/>
+          </div>
+          <Menu onToggleMenu={this.handleToggleMenu}/>
+        </div>
+      </>
+    )
+  }
 }
 
 export default Layout
