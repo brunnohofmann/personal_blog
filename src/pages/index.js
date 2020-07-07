df21e22c0d32c9aeb41a8c14532a4a961ac46884
Brunno Hofmann <brunnohofmann@hotmail.com>
2020-07-07 17:49:29 -0300

    Removes lorem ipsum from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,27 +30,11 @@ const HomeIndex = ({ data }) => {
             <article style={{ backgroundImage: `url(${getImageSrcFromString(post.content.encoded)})` }}>
               <header className="major">
                 <h3>{post.title}</h3>
-                <p>Ipsum dolor sit amet</p>
               </header>
               <Link to={`/${createSlug(post.title)}`} className="link primary" />
             </article>
           ))}
         </section>
-
-        {/*<section id="two">*/}
-        {/*  <div className="inner">*/}
-        {/*    <header className="major">*/}
-        {/*      <h2>Massa libero</h2>*/}
-        {/*    </header>*/}
-        {/*    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed*/}
-        {/*      nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet*/}
-        {/*      massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus*/}
-        {/*      et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>*/}
-        {/*    <ul className="actions">*/}
-        {/*      <li><Link to="/landing" className="button next">Get Started</Link></li>*/}
-        {/*    </ul>*/}
-        {/*  </div>*/}
-        {/*</section>*/}
       </div>
 
     </Layout>
